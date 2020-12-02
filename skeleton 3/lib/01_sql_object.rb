e72a23aa45beafe6df33ffeaf8e3ea5ca3c8d3a6
henryzihaohuang <atlas.bb.contact@gmail.com>
2020-12-02 16:53:47 -0600

    Build Your Own ActiveRecord Part 1

---
+++ skeleton 3/lib/01_sql_object.rb
@@ -0,0 +1,76 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @res if @res 
+
+    columns = DBConnection.execute2(<<-SQL).first
+      SELECT * 
+      FROM #{self.table_name};
+    SQL
+
+    @res = columns.map {|col| col.to_sym}
+  end
+
+  def self.finalize!
+    self.columns.each do |col|
+      define_method(col) do
+        self.attributes[col]
+      end
+
+      define_method("#{col}=") do |value|
+        self.attributes[col] = value
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.to_s.downcase + "s"
+  end
+
+  def self.all
+    
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    params.keys.each do |param|   
+      raise "unknown attribute '#{param}'" unless self.class.columns.include?(param.to_sym) 
+      self.send("#{attr_name}=", value)
+    end
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
